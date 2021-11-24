ad7c355824dc1e0e7de7bb934551c32116060938
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2021-11-25 00:20:48 +0100

    fix cookies maybe?

--- Posts/FirstPost/FirstPost.js
+++ Posts/FirstPost/FirstPost.js
@@ -75,5 +75,5 @@ function SetCookie(cname, cvalue, exdays)
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires;
+    document.cookie = cname + "=" + cvalue + ";" + expires + "; path=/";
 }
