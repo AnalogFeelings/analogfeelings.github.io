85d16a12f34e12d4c44367c6583775d27b66cac3
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-08-06 03:32:29 +0200

    Fix 2000 404

--- 404.js
+++ 404.js
@@ -12,32 +12,12 @@ function CheckCss()
         {
             case "7":
             case "xp":
-                document.getElementById("98style").setAttribute("style", "visibility: hidden;");
-                document.getElementById("XPstyle").removeAttribute("style");
+                document.getElementById("2000style").setAttribute("hidden", true);
+                document.getElementById("XPstyle").removeAttribute("hidden");
                 break;
             default:
             case "2000":
                 break;
         }
     }
-}
-
-function GetCookie(cname) 
-{
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) 
-    {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') 
-        {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) 
-        {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
 }
