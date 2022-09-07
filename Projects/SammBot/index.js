54e70447c9a722116c854f674556379e7618d221
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-09-08 00:02:36 +0200

    [Samm-Bot] Add logo to sticky banner, add drop shadow as well.

--- Projects/SammBot/index.js
+++ Projects/SammBot/index.js
@@ -2,12 +2,22 @@ window.onload = InitBanner;
 
 function InitBanner()
 {
+    let bannerLogoElement = document.getElementById("bannerLogo");
     let stickyBannerElement = document.querySelector('[id="stickyBanner"]');
-    let stickyObserver = new IntersectionObserver(([e]) => e.target.classList.toggle("sticky", e.boundingClientRect.top === 0),
+    let stickyObserver = new IntersectionObserver(([e]) => BannerCallback(e),
         {
             rootMargin: '-1px 0px 0px 0px',
             threshold: [1]
         });
 
     stickyObserver.observe(stickyBannerElement);
+
+    function BannerCallback(e)
+    {
+        let isIntersecting = e.boundingClientRect.top === 0;
+
+        bannerLogoElement.classList.toggle("shown", isIntersecting);
+
+        e.target.classList.toggle("sticky", isIntersecting);
+    }
 }
