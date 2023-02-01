26d9d3e9922ec78d05e8990ff5d0c897431db74e
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2023-02-01 19:22:44 +0100

    Add posts page.

---
+++ posts.js
@@ -0,0 +1,38 @@
+window.onload = PageStartup;
+
+function PageStartup()
+{
+	LoadElements();
+
+	RetrievePosts(LoadPosts);
+
+	window.addEventListener("mouseup", (e) => HandleClick(e));
+}
+
+function LoadPosts(fileContent)
+{
+	let recentPosts = document.getElementById("postsList");
+
+	let i = 0;
+
+	fileContent.forEach(function (currentValue)
+	{
+		//The post you see in the "Recent Posts" part of the home tab.
+		let createdPostRecent = document.createElement("li");
+		let createdPostRecentDesc = document.createElement("p");
+
+		createdPostRecent.classList.add("recentPostHeader");
+		createdPostRecentDesc.classList.add("recentPostDescription");
+
+		createdPostRecent.innerHTML = `<b>${ currentValue.date }</b> - <a href="${ currentValue.url }">${ currentValue.title }</a>`;
+		createdPostRecentDesc.innerHTML = currentValue.description;
+
+		if (i < 8)
+		{
+			recentPosts.appendChild(createdPostRecent);
+			recentPosts.appendChild(createdPostRecentDesc);
+		}
+
+		i++;
+	});
+}
