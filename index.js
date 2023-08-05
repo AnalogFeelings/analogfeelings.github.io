681072336f69a6705172c6c60c607ab4715478e8
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2023-08-05 02:21:54 +0200

    Massively improve home page.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function PageStartup()
 	LoadElements();
 
 	RetrievePosts(true, LoadPosts);
+	RetrievePosts(false, LoadArticles);
 
 	window.addEventListener("mouseup", (e) => HandleClick(e));
 }
@@ -31,6 +32,34 @@ function LoadPosts(fileContent)
 		{
 			recentPosts.appendChild(createdPostRecent);
 			recentPosts.appendChild(createdPostRecentDesc);
+		}
+
+		i++;
+	});
+}
+
+function LoadArticles(fileContent)
+{
+	let recentArticles = document.getElementById("recentArticleList");
+
+	let i = 0;
+
+	fileContent.forEach(function (currentValue)
+	{
+		//The post you see in the "Recent Posts" part of the home tab.
+		let createdArticleRecent = document.createElement("li");
+		let createdArticleRecentDesc = document.createElement("p");
+
+		createdArticleRecent.classList.add("postHeader");
+		createdArticleRecentDesc.classList.add("postDescription");
+
+		createdArticleRecent.innerHTML = `<b>${ currentValue.date }</b> - <a href="${ currentValue.url }">${ currentValue.title }</a>`;
+		createdArticleRecentDesc.innerHTML = currentValue.description;
+
+		if (i < 8)
+		{
+			recentArticles.appendChild(createdArticleRecent);
+			recentArticles.appendChild(createdArticleRecentDesc);
 		}
 
 		i++;
