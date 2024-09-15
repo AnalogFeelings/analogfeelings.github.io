07394de1d50e26bacc36b228f1634551190b4d45
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-09-15 20:49:20 +0200

    Add navlink ads visibility setting.

--- javascript/pages/settings.js
+++ javascript/pages/settings.js
@@ -1,3 +1,6 @@
+const THEME_DROPDOWN_ID = "themeDropdown";
+const NAVLINK_DROPDOWN_ID = "navlinkDropdown";
+
 LoadTheme();
 
 window.onload = PageStartup;
@@ -5,11 +8,57 @@ window.onload = PageStartup;
 async function PageStartup()
 {
 	await LoadElements();
+	LoadNavlink();
 
-	let themeDropdown = document.getElementById("themeDropdown");
+	let themeDropdown = document.getElementById(THEME_DROPDOWN_ID);
+	let navlinkDropdown = document.getElementById(NAVLINK_DROPDOWN_ID);
 
 	themeDropdown.value = GetThemePreference();
 	themeDropdown.addEventListener("change", OnThemeChanged);
 
+	navlinkDropdown.value = GetNavlinkPreference();
+	navlinkDropdown.addEventListener("change", OnNavlinkChanged);
+
 	window.addEventListener("mouseup", (e) => HandleClick(e));
+}
+
+/**
+ * Handles theme selection.
+ */
+function OnThemeChanged()
+{
+	let dropdown = document.getElementById(THEME_DROPDOWN_ID);
+	let value = dropdown.value;
+
+	switch (value)
+	{
+		case LIGHT_THEME:
+		case DARK_THEME:
+		case SYSTEM_THEME:
+			SetThemePreference(value);
+			break;
+		default:
+			SetThemePreference(SYSTEM_THEME);
+			break;
+	}
+}
+
+/**
+ * Handles navlink visibility selection.
+ */
+function OnNavlinkChanged()
+{
+	let dropdown = document.getElementById(NAVLINK_DROPDOWN_ID);
+	let value = dropdown.value;
+
+	switch(value)
+	{
+		case NAVLINK_ENABLED:
+		case NAVLINK_DISABLED:
+				SetNavlinkPreference(value);
+				break;
+		default:
+			SetNavlinkPreference(NAVLINK_ENABLED);
+			break;
+	}
 }
