75a3ba2f16a0239cf26f08038605b7ab4927849f
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-09-18 16:51:52 +0200

    Screensaver knowledge base post.

---
+++ knowledge/writing-a-screensaver/resources/code/threefunctions.cpp
@@ -0,0 +1,14 @@
+LRESULT WINAPI ScreenSaverProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    // ...
+}
+
+BOOL WINAPI RegisterDialogClasses(HANDLE hInst)
+{
+	// ...
+}
+
+BOOL WINAPI ScreenSaverConfigureDialog(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    // ...
+}
